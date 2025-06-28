6f4ff9ddc2801a7873b776436f29c72915adfd95
F4CBR <49267296+F4CBR@users.noreply.github.com>
2025-06-28 11:33:09 +0700

    Update and rename z43nxshell.php to fr4nkshell.php

--- fr4nkshell.php
+++ fr4nkshell.php
@@ -196,7 +196,7 @@ if (isset($_GET['logout'])) {
 <html>
 
    <head>
-      <title>Webshell - ZH43NX</title>
+      <title>Webshell - MRFRANK</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
          integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
